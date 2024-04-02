b0b20092a15d5d19519a1531c9772ae4f1ad534b
Robson Andrade <robsonas@gmail.com>
2024-04-02 10:53:44 -0300

    feat: Implement create song endpoint

--- src/main/app.py
+++ src/main/app.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI
 from src.infra.db.settings.connection import init_db
 from src.main.routes.songs import router as router_songs
 
-from src.infra.db.entities.songs import Song
-
 app = FastAPI()
 
 
--- src/main/routes/songs.py
+++ src/main/routes/songs.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
-from src.infra.db.entities.songs import Song
+from src.infra.db.entities.songs import Song, SongCreate
 from src.infra.db.settings.connection import get_session
 
 router = APIRouter()
@@ -14,3 +14,12 @@ def get_songs(session: Session = Depends(get_session)):
     result = session.execute(select(Song))
     songs = result.scalars().all()
     return [Song(name=song.name, artist=song.artist, id=song.id) for song in songs]
+
+
+@router.post('/songs', status_code=201)
+def create_song(song: SongCreate, session: Session = Depends(get_session)):
+    song = Song(name=song.name, artist=song.artist)
+    session.add(song)
+    session.commit()
+    session.refresh(song)
+    return song
