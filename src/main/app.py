5e28dbbec68125f1dbac2c1a72faed49d87d32f9
Robson Andrade <robsonas@gmail.com>
2024-04-04 16:10:42 -0300

    config: Add CORS config

--- src/main/app.py
+++ src/main/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
-# from src.infra.db.settings.connection import init_db
+from fastapi.middleware.cors import CORSMiddleware
 from src.main.routes.songs import router as router_songs
 
+# from src.infra.db.settings.connection import init_db
+
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 @app.get('/')
